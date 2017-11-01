0c5886482e91984ae2542c14d60ccd3cfafed45a
Rosando Desouza <desouza.rosando@gmail.com>
2017-11-01 23:14:13 +0530

    Added based get route to / and /books

--- LibraryApp/app.js
+++ LibraryApp/app.js
@@ -4,6 +4,16 @@ var app = express(); //create a instance of express
 
 var port = 5000;
 
+app.get('/', function(req, res){
+	console.log(req.url);
+	res.send("Helloworld");
+});
+
+app.get('/books', function(req, res){
+	console.log(req.url);
+	res.send("Hello Books");
+});
+
 app.listen(port, function(){
 	console.log("Server running on port: " + port);
 });

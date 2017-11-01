99cb3284aa9576baeda8e04074340ff5dde769a1
Rosando Desouza <desouza.rosando@gmail.com>
2017-11-02 01:33:55 +0530

    Added package with bower and add nodemon to restart server

--- LibraryApp/app.js
+++ LibraryApp/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express(); //create a instance of express
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 //setup middleware to access the files from public directory
 app.use(express.static('public'));
@@ -11,14 +11,14 @@ app.use(express.static('src/views'));
 
 app.get('/', function(req, res){
 	console.log(req.url);
-	res.send("Helloworld");
+	res.send('Helloworld');
 });
 
 app.get('/books', function(req, res){
 	console.log(req.url);
-	res.send("Hello Books");
+	res.send('Hello Books');
 });
 
 app.listen(port, function(){
-	console.log("Server running on port: " + port);
+	console.log('Server running on port: ' + port);
 });

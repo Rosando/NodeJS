7e24f66745b64d6d976ed54956d9f7a8610329c3
Rosando Desouza <desouza.rosando@gmail.com>
2017-11-02 09:40:17 +0530

    Added jade view to display home page

--- LibraryApp/app.js
+++ LibraryApp/app.js
@@ -7,11 +7,18 @@ var port = process.env.PORT || 5000;
 //setup middleware to access the files from public directory
 app.use(express.static('public'));
 
-app.use(express.static('src/views'));
+//app.use(express.static('src/views'));
+
+//set the views vairable to path where the views will be stored
+app.set('views', './src/views');
+
+//using jade templating engine
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-	console.log(req.url);
-	res.send('Helloworld');
+	//console.log(req.url);
+	//res.send('Helloworld');
+	res.render('./jade/index', {list:['1','2','3','4','5']});
 });
 
 app.get('/books', function(req, res){

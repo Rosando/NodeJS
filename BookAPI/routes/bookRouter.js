45b489ecb950a7ee18470a87159d1dbdd0ec2576
Rosando Desouza <desouza.rosando@gmail.com>
2017-11-13 01:49:39 +0530

    Added api method for put, patch and delete

--- BookAPI/routes/bookRouter.js
+++ BookAPI/routes/bookRouter.js
@@ -42,15 +42,65 @@ var bookRouterContainer = function(Book){
 				});
 			});
 
+	bookRouter.use('/:bookId', function(req, res, next){
+		Book.findById(req.params.bookId, function(err, book){
+			if(err)
+				res.status(500).send(err);
+			
+			if(book)
+			{
+				req.book = book;
+				next();
+			}
+			else
+				res.status(404).send('Book not found');
+		});
+	});
+	
 	bookRouter.route('/:bookId')
 			.get(function(req, res){
 				//console.log(req.params.bookId
+				res.json(req.book);
+			})
+			.put(function(req, res){
+				var book = req.book;
 				
-				Book.findById(req.params.bookId, function(err, book){
+				book.title = req.body.title;
+				book.author = req.body.author;
+				book.genre = req.body.genre;
+				book.read = req.body.read;
+				
+				book.save(function(err){
 					if(err)
 						res.status(500).send(err);
 					else
 						res.json(book);
+				});
+			})
+			.patch(function(req, res){
+				if(req.book._id)
+					delete req.body._id;
+				
+				if(req.book.__v)
+					delete req.body.__v;
+				
+				for(var item in req.body){
+					req.book[item] = req.body[item]
+				}
+				
+				req.book.save(function(err){
+					if(err)
+						res.status(500).send(err);
+					else
+						res.json(req.book);
+				});
+			})
+			.delete(function(req, res){
+				req.book.remove(function(err){
+					if(err)
+						res.status(500).send(err);
+					else
+						res.status(204).send('Book Deleted');
 				});
 			});
 			
